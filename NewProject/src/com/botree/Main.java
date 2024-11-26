6df1f11c607c68a569292f3c2bf116b4eada8bf6
diwakar <diwakarpandey.2002@gmail.com>
2024-11-26 14:30:19 +0530

    Second edit

--- NewProject/src/com/botree/Main.java
+++ NewProject/src/com/botree/Main.java
@@ -3,5 +3,7 @@ package com.botree;
 public class Main {
 public static void main(String[] args) {
 	System.out.println("work here");
+	Service s= new Service();
+	System.out.println(s.sum(10, 20, 30));
 }
 }
